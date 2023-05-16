3bb98f320189395ac08dae1f8e6e9a9cea2d4abc
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2023-05-16 13:54:43 +1200

    Enable crashlytics

--- pantry_pal_app/lib/main.dart
+++ pantry_pal_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pantry_pal/features/auth/login_page.dart';
@@ -17,6 +19,13 @@ Future main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(MultiProvider(
     providers: [
